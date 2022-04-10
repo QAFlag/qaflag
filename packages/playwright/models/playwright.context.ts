fe3c8ac26b2c327a91f79cf9d5689d6ece04bd77
Jason Byrne <jbyrne@echelonfit.com>
2022-04-10 11:28:10 -0400

    add soe more playwright wrapper methods

--- packages/playwright/models/playwright.context.ts
+++ packages/playwright/models/playwright.context.ts
@@ -1,7 +1,24 @@
-import { Context, ContextInterface, ScenarioInterface } from '@qaflag/core';
-import { Locator } from 'playwright';
+import {
+  Context,
+  ContextInterface,
+  ScenarioInterface,
+  StringValue,
+} from '@qaflag/core';
+import { Locator, PageScreenshotOptions } from 'playwright';
 import { PlaywrightInstance } from './playwright.adapter';
 import { PlaywrightValue } from './playwright.value';
+
+export type NavigationOpts =
+  | {
+      timeout?: number | undefined;
+      waitUntil?:
+        | 'load'
+        | 'domcontentloaded'
+        | 'networkidle'
+        | 'commit'
+        | undefined;
+    }
+  | undefined;
 
 export type FindOpts = {
   has?: Locator;
@@ -14,6 +31,13 @@ export class PlaywrightContext extends Context implements ContextInterface {
     protected readonly playwright: PlaywrightInstance,
   ) {
     super(scenario);
+  }
+
+  public get url() {
+    return new StringValue(this.page.url(), {
+      name: 'URL',
+      logger: this,
+    });
   }
 
   public get page() {
@@ -39,5 +63,25 @@ export class PlaywrightContext extends Context implements ContextInterface {
     const locator = this.find(selector, opts);
     await locator.must.exist();
     return locator;
+  }
+
+  public async screenshot(opts: PageScreenshotOptions) {
+    return this.page.screenshot(opts);
+  }
+
+  public async reload(opts: NavigationOpts) {
+    return this.page.reload(opts);
+  }
+
+  public async goForward(opts: NavigationOpts) {
+    return this.page.goForward(opts);
+  }
+
+  public async goBack(opts: NavigationOpts) {
+    return this.page.goBack(opts);
+  }
+
+  public async goTo(url: string, opts: NavigationOpts) {
+    return this.page.goto(url, opts);
   }
 }
