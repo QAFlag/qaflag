35845681491bfeec68f867ee4f87da5aeaf94eba
Jason Byrne <jbyrne@echelonfit.com>
2022-05-14 17:41:44 -0400

    find within jsonvalue

--- packages/json/models/json.value.ts
+++ packages/json/models/json.value.ts
@@ -2,6 +2,7 @@ import { ValueAbstract } from '@qaflag/core';
 import { JsonMust } from '../types/json-test.interface';
 import { JsonData } from '../types/json-data';
 import { JsonAssertion } from './json.assertion';
+import * as jmespath from 'jmespath';
 
 export class JsonValue extends ValueAbstract<JsonData> {
   public get must(): JsonMust {
@@ -10,5 +11,10 @@ export class JsonValue extends ValueAbstract<JsonData> {
 
   public get should(): JsonMust {
     return new JsonAssertion(this, 'should');
+  }
+
+  public find(selector: string) {
+    const results: JsonData = jmespath.search(this.input, selector);
+    return new JsonValue(results, { name: selector, logger: this.logger });
   }
 }
