b58870af6335ba10cd40b3b440a97bf4b5ee0cf6
Jason Byrne <jbyrne@echelonfit.com>
2022-10-20 15:29:45 -0400

    oops forgot to export it

--- packages/playwright/index.ts
+++ packages/playwright/index.ts
@@ -5,3 +5,4 @@ export * from './models/playwright.request';
 export * from './models/playwright.adapter';
 export * from './models/bounding-box.value';
 export * from './models/playwright.assertion';
+export * from './utils/exists-as';
